f51c658a86d97055c6ad4a63fc53f1782db5623e
SunhyeokChoe <chltnsgur2@daum.net>
2022-03-04 20:27:46 +0900

    feat(header): add facebook icon, searchbar

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,37 @@
 import Image from 'next/image'
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGroupIcon,
+} from '@heroicons/react/solid'
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from '@heroicons/react/outline'
 
 function Header() {
   return (
     <div>
-      <div>Header</div>
-
       {/* Left */}
-      <div>
+      <div className="flex items-center">
         <Image
           src="https://links.papareact.com/5me"
           width={40}
           height={40}
           layout="fixed"
         />
-        <div>
-          <input type="text" placeholder="Search Facebook" />
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <SearchIcon className="h-6 text-gray-600" />
+          <input
+            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
+            type="text"
+            placeholder="Search Facebook"
+          />
         </div>
       </div>
 
