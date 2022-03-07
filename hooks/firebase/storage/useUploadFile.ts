aaa017100df79ba969032096d0a1de4853f700ed
SunhyeokChoe <chltnsgur2@daum.net>
2022-03-08 03:14:28 +0900

    chore: add comments to useUploadFile hooks

--- hooks/firebase/storage/useUploadFile.ts
+++ hooks/firebase/storage/useUploadFile.ts
@@ -8,7 +8,23 @@ import {
 } from 'firebase/storage'
 import { useMemo, useState } from 'react'
 
+/**
+ * An enumeration of the possible states of the upload task.
+ * @public
+ * @param function - {@link StorageReference} where data should be uploaded.
+ * @param data - The data to upload.
+ * @param metadata - Metadata for the data to upload.
+ * @returns A Promise containing an {@link UploadResult} | undefined. The snapshot of the upload task.
+ */
 export type UploadFileHook = [
+  /**
+   * An enumeration of the possible states of the upload task.
+   * @public
+   * @param storageRef - {@link StorageReference} where data should be uploaded.
+   * @param data - The data to upload.
+   * @param metadata - Metadata for the data to upload.
+   * @returns An {@link Promise<UploadResult | undefined>} - The snapshot of the upload task.
+   */
   (
     storageRef: StorageReference,
     data: Blob | Uint8Array | ArrayBuffer,
@@ -20,12 +36,15 @@ export type UploadFileHook = [
 ]
 
 /**
- * Uploads a file to a storage reference.
+ * Uploads data to object's location(reference).
+ * The upload can be paused and resumed, and exposes progress updates.
+ * @public
+ * @returns An {@link UploadFileHook}
  */
 export default (): UploadFileHook => {
-  const [error, setError] = useState<StorageError>()
   const [uploading, setUploading] = useState<boolean>(false)
   const [snapshot, setSnapshot] = useState<UploadTaskSnapshot>()
+  const [error, setError] = useState<StorageError>()
 
   const uploadFile = async (
     storageRef: StorageReference,
