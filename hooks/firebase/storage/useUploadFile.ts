d8c06a4c3bd00cd358889be4a30cf81c4fefd7e4
SunhyeokChoe <chltnsgur2@daum.net>
2022-03-08 01:37:30 +0900

    feat(util): add hooks for upload/download files to Firebase Cloud Storage

---
+++ hooks/firebase/storage/useUploadFile.ts
@@ -0,0 +1,66 @@
+import {
+  StorageError,
+  StorageReference,
+  uploadBytesResumable,
+  UploadMetadata,
+  UploadResult,
+  UploadTaskSnapshot,
+} from 'firebase/storage'
+import { useMemo, useState } from 'react'
+
+export type UploadFileHook = [
+  (
+    storageRef: StorageReference,
+    data: Blob | Uint8Array | ArrayBuffer,
+    metadata?: UploadMetadata | undefined,
+  ) => Promise<UploadResult | undefined>,
+  boolean,
+  UploadTaskSnapshot | undefined,
+  StorageError | undefined,
+]
+
+/**
+ * Uploads a file to a storage reference.
+ */
+export default (): UploadFileHook => {
+  const [error, setError] = useState<StorageError>()
+  const [uploading, setUploading] = useState<boolean>(false)
+  const [snapshot, setSnapshot] = useState<UploadTaskSnapshot>()
+
+  const uploadFile = async (
+    storageRef: StorageReference,
+    data: Blob | Uint8Array | ArrayBuffer,
+    metadata?: UploadMetadata | undefined,
+  ): Promise<UploadResult | undefined> => {
+    return new Promise((resolve, reject) => {
+      setUploading(true)
+      setError(undefined)
+      const uploadTask = uploadBytesResumable(storageRef, data, metadata)
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          setSnapshot(snapshot)
+        },
+        (error) => {
+          setUploading(false)
+          setError(error)
+          resolve(undefined)
+        },
+        () => {
+          setUploading(false)
+          setSnapshot(undefined)
+          resolve({
+            metadata: uploadTask.snapshot.metadata,
+            ref: uploadTask.snapshot.ref,
+          })
+        },
+      )
+    })
+  }
+
+  /**
+   * Resets the state of the hook.
+   */
+  const resArray: UploadFileHook = [uploadFile, uploading, snapshot, error]
+  return useMemo<UploadFileHook>(() => resArray, resArray)
+}
