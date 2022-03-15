b32ff712240f048d04b5f1366cd19f4d9d217be3
SunhyeokChoe <chltnsgur2@daum.net>
2022-03-16 06:06:43 +0900

    feat(Feed, hooks): add Posts component, hooks related to firestore

---
+++ components/Posts.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import { useCollection } from '@hooks/firebase/firestore/useCollection'
+
+const Posts: React.FC = (): JSX.Element => {
+  return <div></div>
+}
+
+export default Posts
