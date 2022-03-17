7d65d52d098864402242803a4494b1dd4f9d9292
SunhyeokChoe <chltnsgur2@daum.net>
2022-03-18 00:38:49 +0900

    feat(Feed): add header, body to the Post component

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Image from 'next/image'
 
 type IProps = {
   name: string
   message: String
   email: string
-  timestamp: Date
+  timestamp: any
   image: string
   postImage: string
 }
@@ -18,13 +19,32 @@ const Post: React.FC<IProps> = ({
   postImage,
 }): JSX.Element => {
   return (
-    <div>
-      <h1>{name}</h1>
-      <h1>{message}</h1>
-      <h1>{email}</h1>
-      {/* <h1>{timestamp}</h1> */}
-      <h1>{image}</h1>
-      <h1>{postImage}</h1>
+    <div className="flex flex-col">
+      <div className="p-5 bg-white mt-5 rounded-t-2xl shadow-sm">
+        <div className="flex space-x-2">
+          <img
+            className="rounded-full"
+            src={image}
+            alt="image"
+            width={40}
+            height={40}
+          />
+          <div>
+            <p className="font-medium ">{name}</p>
+            <p className="text-xs text-gray-400">
+              {new Date(timestamp?.toDate()).toLocaleString()}
+            </p>
+          </div>
+        </div>
+
+        <p className="pt-4">{message}</p>
+      </div>
+
+      {postImage && (
+        <div className="relative h-56 md:h-96 bg-white">
+          <Image src={postImage} objectFit="cover" layout="fill" />
+        </div>
+      )}
     </div>
   )
 }
