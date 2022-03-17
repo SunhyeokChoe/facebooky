9b3ed7639da0a33332ca167f40b4ebd771edd948
SunhyeokChoe <chltnsgur2@daum.net>
2022-03-18 00:53:34 +0900

    feat(Feed): add footer to the post compoent

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import { ThumbUpIcon, ChatAltIcon, ShareIcon } from '@heroicons/react/outline'
 
 type IProps = {
   name: string
@@ -21,16 +22,17 @@ const Post: React.FC<IProps> = ({
   return (
     <div className="flex flex-col">
       <div className="p-5 bg-white mt-5 rounded-t-2xl shadow-sm">
+        {/* Header */}
         <div className="flex space-x-2">
           <img
             className="rounded-full"
             src={image}
-            alt="image"
+            alt="profile image"
             width={40}
             height={40}
           />
           <div>
-            <p className="font-medium ">{name}</p>
+            <p className="font-medium">{name}</p>
             <p className="text-xs text-gray-400">
               {new Date(timestamp?.toDate()).toLocaleString()}
             </p>
@@ -40,11 +42,32 @@ const Post: React.FC<IProps> = ({
         <p className="pt-4">{message}</p>
       </div>
 
+      {/* Body: image */}
       {postImage && (
         <div className="relative h-56 md:h-96 bg-white">
           <Image src={postImage} objectFit="cover" layout="fill" />
         </div>
       )}
+
+      <div
+        className="flex justify-between item-center rounded-b-2xl bg-white
+        shadow-md text-gray-400 border-t pl-2 pr-2"
+      >
+        <div className="inputIcon rounded-none rounded-bl-2xl">
+          <ThumbUpIcon className="h-4" />
+          <p className="text-xs sm:text-base">Like</p>
+        </div>
+
+        <div className="inputIcon rounded-none">
+          <ChatAltIcon className="h-4" />
+          <p className="text-xs sm:text-base">Comment</p>
+        </div>
+
+        <div className="inputIcon rounded-none rounded-br-2xl">
+          <ShareIcon className="h-4" />
+          <p className="text-xs sm:text-base">Comment</p>
+        </div>
+      </div>
     </div>
   )
 }
